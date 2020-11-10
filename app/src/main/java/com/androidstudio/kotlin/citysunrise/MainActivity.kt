f20860e4174e171d537e05736e0531caafddb75d
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-10 23:19:50 +0530

    create getSunset function

--- app/src/main/java/com/androidstudio/kotlin/citysunrise/MainActivity.kt
+++ app/src/main/java/com/androidstudio/kotlin/citysunrise/MainActivity.kt
@@ -2,10 +2,16 @@ package com.androidstudio.kotlin.citysunrise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    protected fun getSunset(view:View){
+        var city = ""
+        val url = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="+ city +",ca&format=json" ;
     }
 }
