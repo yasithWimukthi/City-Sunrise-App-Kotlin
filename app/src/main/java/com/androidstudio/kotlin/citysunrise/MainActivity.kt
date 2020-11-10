68c285d3b3e7bebff4eb2fd4564ee8ea8e4b1263
yasithWimukthi <it19966922@my.sliit.lk>
2020-11-10 23:57:23 +0530

    create getData inner class

--- app/src/main/java/com/androidstudio/kotlin/citysunrise/MainActivity.kt
+++ app/src/main/java/com/androidstudio/kotlin/citysunrise/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.androidstudio.kotlin.citysunrise
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,5 +15,33 @@ class MainActivity : AppCompatActivity() {
     protected fun getSunset(view:View){
         var city = cityName.text.toString()
         val url = "https://weather-ydn-yql.media.yahoo.com/forecastrss?location="+ city +",ca&format=json" ;
+
+        getData().execute(url)
+    }
+
+    inner class getData: AsyncTask<String, String, String>() {
+        override fun doInBackground(vararg params: String?): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+        }
+
+        override fun onProgressUpdate(vararg values: String?) {
+            super.onProgressUpdate(*values)
+        }
+
+        override fun onCancelled(result: String?) {
+            super.onCancelled(result)
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+        }
     }
 }
